07787cf56fcb8f2bff1b0adb79ce1dd0a4a8d10d
Jean Marc Doxaran <cdoxaran@gmail.com>
2024-09-08 21:44:38 +0200

    prix

--- shop/settings.py
+++ shop/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL ="/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+#MEDIA_ROOT = BASE_DIR / "media"
 django_heroku.settings(locals())
 
 AUTH_USER_MODEL = "comptes.Utilisateur"
